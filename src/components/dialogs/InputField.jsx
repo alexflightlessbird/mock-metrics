8c9d0d96e1b84b9c8ec28b98f00c31e93f1c47fb
Alex <102932289+alexflightlessbird@users.noreply.github.com>
2025-03-12 20:29:32 -0400

    dialog changes

--- src/components/dialogs/InputField.jsx
+++ src/components/dialogs/InputField.jsx
@@ -9,7 +9,7 @@ export default function InputField({
   disabled,
   required,
   readonly,
-  autoFocus,
+  autofocus,
   min,
   max,
   step,
@@ -22,77 +22,135 @@ export default function InputField({
   autocomplete,
   options,
   checked,
+  description,
+  optionsGrouped,
 }) {
   switch (type) {
     case "checkbox":
       return (
-        <div>
-          <label htmlFor={id}>{label}</label>
-          <br />
-          <input
-            type={type}
-            id={id}
-            name={id}
-            checked={value || checked}
-            onChange={(e) => onChange(id, e.target.checked)}
-            disabled={disabled}
-            required={required}
-            readOnly={readonly}
-            autoFocus={autoFocus}
-          />
+        <div className="input">
+          <label htmlFor={id}>
+            {label}
+            {description && <p>{description}</p>}
+            <input
+              type={type}
+              id={id}
+              name={id}
+              checked={checked}
+              onChange={(e) => onChange(id, e.target.value)}
+              disabled={disabled}
+              required={required}
+              readOnly={readonly}
+              autoFocus={autofocus}
+            />
+          </label>
         </div>
       );
     case "radio":
       return (
-        <div>
-          <label htmlFor={id}>{label}</label>
-          <br />
-          {options.map((o, j) => (
-            <div key={j}>
-              <label htmlFor={o.id}>{o.label}</label>
-              <br />
-              <input
-                type={type}
-                id={o.id}
-                name={id}
-                value={o.value}
-                checked={value === o.value}
-                onChange={(e) => onChange(id, e.target.value)}
-                disabled={o.disabled}
-                required={required}
-                readOnly={o.readonly}
-                autoFocus={autoFocus}
-              />
-            </div>
-          ))}
+        <div className="input">
+          <label id={id}>
+            {label}
+            {description && <p>{description}</p>}
+            {options.map((o, j) => (
+              <div key={j}>
+                <label htmlFor={o.id}>
+                  {o.label}
+                  <input
+                    title={o.label}
+                    type={type}
+                    id={o.id}
+                    name={id}
+                    value={o.value}
+                    onChange={(e) => onChange(id, e.target.value)}
+                    disabled={o.disabled}
+                    required={required}
+                    readOnly={o.readonly}
+                    autoFocus={autofocus}
+                  />
+                </label>
+              </div>
+            ))}
+          </label>
         </div>
+      );
+    case "select":
+      return (
+        <div className="input">
+          <label htmlFor={id}>
+            {label}
+            {description && <p>{description}</p>}
+            <select name={id} id={id}>
+              {optionsGrouped.map((g, i) => (
+                <optgroup key={i} label={g.label}>
+                  {g.options.map((o, j) => (
+                    <option
+                      key={j}
+                      value={o.value}
+                      disabled={o.disabled}
+                      selected={o.selected}
+                    >
+                      {o.label}
+                    </option>
+                  ))}
+                </optgroup>
+              ))}
+            </select>
+          </label>
+        </div>
+      );
+    case "hidden":
+      return (
+        <input
+          type={type}
+          id={id}
+          name={id}
+          value={value || ""}
+          onChange={(e) => onChange(id, e.target.value)}
+          min={min}
+          max={max}
+          step={step}
+          pattern={pattern}
+          title={title}
+          placeholder={placeholder}
+          size={size}
+          maxLength={maxLength}
+          multiple={multiple}
+          disabled={disabled}
+          required={required}
+          readOnly={readonly}
+          autoFocus={autofocus}
+          autoComplete={autocomplete}
+        />
       );
     default:
       return (
-        <div>
-          <label htmlFor={id}>{label}</label>
-          <br />
-          <input
-            type={type}
-            id={id}
-            name={id}
-            value={value || ""}
-            onChange={(e) => onChange(id, e.target.value)}
-            min={min}
-            max={max}
-            step={step}
-            pattern={pattern}
-            title={title}
-            placeholder={placeholder}
-            size={size}
-            maxLength={maxLength}
-            multiple={multiple}
-            disabled={disabled}
-            required={required}
-            readOnly={readonly}
-            autoFocus={autoFocus}
-            autoComplete={autocomplete}
-          />
+        <div className="input">
+          <label htmlFor={id}>
+            {label}
+            {description && <p>{description}</p>}
+            <input
+              type={type}
+              id={id}
+              name={id}
+              value={value || ""}
+              onChange={(e) => onChange(id, e.target.value)}
+              min={min}
+              max={max}
+              step={step}
+              pattern={pattern}
+              title={title}
+              placeholder={placeholder}
+              size={size}
+              maxLength={maxLength}
+              multiple={multiple}
+              disabled={disabled}
+              required={required}
+              readOnly={readonly}
+              autoFocus={autofocus}
+              autoComplete={autocomplete}
+            />
+          </label>
         </div>
       );
   }
