923611f3ef44c6bcde6a573e66d34beaaa07b2d2
Alex <102932289+alexflightlessbird@users.noreply.github.com>
2025-03-14 16:02:08 +0000

    update file location of ContextProvider

--- src/hooks/auth/useSession.js
+++ src/hooks/auth/useSession.js
@@ -1,4 +1,4 @@
 import { useContext } from "react";
-import { SessionContext } from "../../context/SessionContext";
+import { SessionContext } from "../../context/auth/SessionContext";
 
 export const useSession = () => useContext(SessionContext);
