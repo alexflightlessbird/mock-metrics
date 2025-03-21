81028425536a4ac941872f73ea695f6e6f1dd083
Alex <102932289+alexflightlessbird@users.noreply.github.com>
2025-03-21 23:17:26 +0000

    start setup for settings

--- src/pages/UserSettings.jsx
+++ src/pages/UserSettings.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { supabase } from "../services/supabaseClient";
+import { useSession } from "../hooks/auth/useSession";
+import { setDocumentTitle } from "../utils/helpers";
+import Modal from "../components/paginating-forms/Modal";
 
 export default function UserSettings () {
+    const { userId } = useSession();
+
     return (
         <div>
             <h1>Settings Page</h1>
