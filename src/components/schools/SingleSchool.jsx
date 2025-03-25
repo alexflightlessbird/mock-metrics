30e7efcd9ff846add4d4f5c8a1836a89eeb66d4d
Alex <102932289+alexflightlessbird@users.noreply.github.com>
2025-03-24 21:12:30 -0400

    tournament setup for schools

--- src/components/schools/SingleSchool.jsx
+++ src/components/schools/SingleSchool.jsx
@@ -3,68 +3,97 @@ import { supabase } from "../../services/supabaseClient";
 import List from "../common/List";
 import TeamList from "./TeamList";
 import StudentList from "./StudentList";
+import TournamentList from "./TournamentList";
 
 export default function SingleSchool({ selectedSchool }) {
-    const [allTeams, setAllTeams] = useState([]);    
-    const [allStudents, setAllStudents] = useState([]);
+  const [allTeams, setAllTeams] = useState([]);
+  const [allStudents, setAllStudents] = useState([]);
+  const [allTournaments, setAllTournaments] = useState([]);
 
-    const [activeTeams, inactiveTeams] = useMemo(() => {
-        const active = allTeams.filter((t) => t.is_active);
-        const inactive = allTeams.filter((t) => !t.is_active);
-        return [active, inactive];
-    }, [allTeams]);
+  const [activeTeams, inactiveTeams] = useMemo(() => {
+    const active = allTeams.filter((t) => t.is_active);
+    const inactive = allTeams.filter((t) => !t.is_active);
+    return [active, inactive];
+  }, [allTeams]);
 
-    const [activeStudents, inactiveStudents] = useMemo(() => {
-        const active = allStudents.filter((s) => s.is_active);
-        const inactive = allStudents.filter((s) => !s.is_active);
-        return [active, inactive];
-    }, [allStudents]);
+  const [activeStudents, inactiveStudents] = useMemo(() => {
+    const active = allStudents.filter((s) => s.is_active);
+    const inactive = allStudents.filter((s) => !s.is_active);
+    return [active, inactive];
+  }, [allStudents]);
 
-    useEffect(() => {
-        const fetchTeams = async () => {
-            const { data, error } = await supabase
-                .from("teams")
-                .select("*")
-                .eq("school_id", selectedSchool.schools.id)
-                .order("name");
-            if (error) console.error("Error fetching schools:", error);
-            else setAllTeams(data);
-        }
+  const [activeTournaments, inactiveTournaments] = useMemo(() => {
+    const active = allTournaments.filter((t) => t.is_active);
+    const inactive = allTournaments.filter((t) => !t.is_active);
+    return [active, inactive];
+  }, [allTournaments]);
 
-        const fetchStudents = async () => {
-            const { data, error } = await supabase
-                .from("students")
-                .select("*")
-                .eq("school_id", selectedSchool.schools.id)
-                .order("name");
-            if (error) console.error("Error fetching students:", error);
-            else setAllStudents(data);
-        }
-        fetchTeams();
-        fetchStudents();
-    }, []);
+  useEffect(() => {
+    const fetchTeams = async () => {
+      const { data, error } = await supabase
+        .from("teams")
+        .select("*")
+        .eq("school_id", selectedSchool.schools.id)
+        .order("name");
+      if (error) console.error("Error fetching schools:", error);
+      else setAllTeams(data);
+    };
 
-    const detailItems = [
-        `Short Name: ${selectedSchool.schools.short_name}`,
-        `Premium Status: ${selectedSchool.schools.is_premium ? "Active" : "Inactive"}`,
-        `Your Role: ${selectedSchool.role}`
-    ];
+    const fetchStudents = async () => {
+      const { data, error } = await supabase
+        .from("students")
+        .select("*")
+        .eq("school_id", selectedSchool.schools.id)
+        .order("name");
+      if (error) console.error("Error fetching students:", error);
+      else setAllStudents(data);
+    };
 
-    return (
-        <>
-            <h1>{selectedSchool.schools.name}</h1>
-            <h2>School Details</h2>
-            <List items={detailItems} />
-            <h2>Teams</h2>
-            <h3>Active Teams</h3>
-            <TeamList teams={activeTeams} />
-            <h3>Inactive Teams</h3>
-            <TeamList teams={inactiveTeams} />
-            <h2>Students</h2>
-            <h3>Active Students</h3>
-            <StudentList students={activeStudents} />
-            <h3>Inactive Students</h3>
-            <StudentList students={inactiveStudents} />
-        </>
-    )
+    const fetchTournaments = async () => {
+      const { data, error } = await supabase
+        .from("tournaments")
+        .select("*")
+        .eq("school_id", selectedSchool.schools.id)
+        .order("name");
+      if (error) console.error("Error fetching tournaments:", error);
+      else setAllTournaments(data);
+    };
+
+    fetchTeams();
+    fetchStudents();
+    fetchTournaments();
+  }, []);
+
+  const detailItems = [
+    `Short Name: ${selectedSchool.schools.short_name}`,
+    `Premium Status: ${
+      selectedSchool.schools.is_premium ? "Active" : "Inactive"
+    }`,
+    `Your Role: ${
+      selectedSchool.role === "Primary" ? "Primary Admin" : selectedSchool.role
+    }`,
+  ];
+
+  return (
+    <>
+      <h1>{selectedSchool.schools.name}</h1>
+      <h2>School Details</h2>
+      <List items={detailItems} />
+      <h2>Teams</h2>
+      <h3>Active Teams</h3>
+      <TeamList teams={activeTeams} />
+      <h3>Inactive Teams</h3>
+      <TeamList teams={inactiveTeams} />
+      <h2>Students</h2>
+      <h3>Active Students</h3>
+      <StudentList students={activeStudents} />
+      <h3>Inactive Students</h3>
+      <StudentList students={inactiveStudents} />
+      <h2>Tournaments</h2>
+      <h3>Active Tournaments</h3>
+      <TournamentList tournaments={activeTournaments} />
+      <h3>Inactive Tournaments</h3>
+      <TournamentList tournaments={inactiveTournaments} />
+    </>
+  );
 }
