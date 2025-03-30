401ded45ccfe3719cef19baf2a716f0ee2c2fa7e
Alex <102932289+alexflightlessbird@users.noreply.github.com>
2025-03-29 21:36:09 -0400

    add timeout

--- src/common/components/Loading.jsx
+++ src/common/components/Loading.jsx
@@ -2,11 +2,14 @@ import { Loader, useMantineTheme } from "@mantine/core";
 
 export default function Loading() {
   const theme = useMantineTheme();
-  return (
-    <Loader
-      className="main-loading"
-      color={theme.colors.primaryBlue[0]}
-      type="bars"
-    />
-  );
+
+  setTimeout(() => {
+    return (
+      <Loader
+        className="main-loading"
+        color={theme.colors.primaryBlue[0]}
+        type="bars"
+      />
+    );
+  }, 700);
 }
