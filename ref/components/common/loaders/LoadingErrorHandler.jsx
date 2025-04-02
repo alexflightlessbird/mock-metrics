720d041b44c8344c6f3999582fe78e603e9aee9f
Alex <102932289+alexflightlessbird@users.noreply.github.com>
2025-04-02 14:37:30 -0400

    remove old ref docs

--- ref/components/common/loaders/LoadingErrorHandler.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import LoadingSpinner from "./LoadingSpinner";
-
-export default function LoadingErrorHandler({ loading, error, children }) {
-    if (loading) return <LoadingSpinner />;
-    if (error) return <p>{error}</p>;
-    return children;
-}
