af6ab14a41c85893cedf7b88bfb90819a0e64009
Alex <102932289+alexflightlessbird@users.noreply.github.com>
2025-04-03 14:40:44 -0400

    Update VerificationSuccessView.jsx

--- src/features/auth/views/VerificationSuccessView.jsx
+++ src/features/auth/views/VerificationSuccessView.jsx
@@ -1,18 +1,19 @@
 // Dependency imports
 import { useNavigate } from "react-router-dom";
-import { Text } from "@mantine/core";
+import { Text, useMantineTheme } from "@mantine/core";
 
 // Component imports
 import IconButton from "../../../common/components/IconButton";
 
 export default function VerificationSuccessView() {
     const navigate = useNavigate();
+    const theme = useMantineTheme();
 
     return (
         <>
             <h1>Email Verified</h1>
             <Text>Your email has been successfully verified. You can now log in to your account.</Text>
-            <IconButton onClick={() => navigate("/auth")} buttonText="Go to Login" variant="subtle" fontColor="green" />
+            <IconButton onClick={() => navigate("/auth")} icon="login" buttonText="Go to Login" variant="subtle" fontColor={theme.colors.primaryBlue[0]} />
         </>
     )
 }
