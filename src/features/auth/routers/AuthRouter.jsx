acaf2e4863ad0c3bb6d56156018e512004ed86c6
Alex <102932289+alexflightlessbird@users.noreply.github.com>
2025-04-03 10:38:44 -0400

    auth views

--- src/features/auth/routers/AuthRouter.jsx
+++ src/features/auth/routers/AuthRouter.jsx
@@ -1,9 +1,19 @@
+// Dependency imports
+import { useState } from "react";
+
 // Component imports
 import LoginView from "../views/LoginView";
 import LogoutView from "../views/LogoutView";
 import RegisterView from "../views/RegisterView";
 
 export default function AuthRouter({ session }) {
-    if (!session) return <LoginView />;
+    const [currentView, setCurrentView] = useState("login");
+
+    if (!session) {
+        return currentView === "login"
+            ? <LoginView onToggleView={() => setCurrentView("register")} />
+            : <RegisterView onToggleView={() => setCurrentView("login")} />;
+    };
+    
     return <LogoutView />;
 }
