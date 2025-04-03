2048722d1f4ba140df833878aa8830df4edff248
Alex <102932289+alexflightlessbird@users.noreply.github.com>
2025-04-03 15:01:04 -0400

    button size change and useEffect dependency

--- src/features/auth/views/VerificationSuccessView.jsx
+++ src/features/auth/views/VerificationSuccessView.jsx
@@ -1,4 +1,5 @@
 // Dependency imports
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Text, useMantineTheme } from "@mantine/core";
 
@@ -9,11 +10,18 @@ export default function VerificationSuccessView() {
     const navigate = useNavigate();
     const theme = useMantineTheme();
 
+    useEffect(() => {
+        navigate("/auth?type=email-verification");
+    }, []);
+
     return (
         <>
             <h1>Email Verified</h1>
             <Text>Your email has been successfully verified. You can now log in to your account.</Text>
-            <IconButton onClick={() => navigate("/auth")} icon="login" buttonText="Go to Login" variant="subtle" fontColor={theme.colors.primaryBlue[0]} />
+            <br />
+            <div>
+                <IconButton onClick={() => navigate("/auth")} icon="login" buttonText="Go to Login" variant="outline" fontColor={theme.colors.primaryBlue[0]} />
+            </div>
         </>
     )
 }
