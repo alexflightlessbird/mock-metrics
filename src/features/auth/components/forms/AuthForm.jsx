e90c4702cd430ce95d78522cc9e0340713d2590d
Alex <102932289+alexflightlessbird@users.noreply.github.com>
2025-04-03 10:28:47 -0400

    auth setup

---
+++ src/features/auth/components/forms/AuthForm.jsx
@@ -0,0 +1,49 @@
+// Dependency imports
+import { TextInput, PasswordInput, Alert } from "@mantine/core";
+
+// Component imports
+import IconButton from "../../../../common/components/IconButton";
+
+export default function AuthForm({
+    form,
+    onSubmit,
+    isLoading,
+    error,
+    submitLabel = "Submit",
+    showConfirmPassword = false
+}) {
+    return (
+        <form onSubmit={form.onSubmit(onSubmit)}>
+            {error && (
+                <Alert title="Error" color="red" mb="md">{error}</Alert>
+            )}
+
+            <TextInput
+                label="Email"
+                placeholder="your@email.com"
+                withAsterisk
+                {...form.getInputProps("email")}
+            />
+            <PasswordInput
+                label="Password"
+                placeholder="Your password"
+                withAsterisk
+                mt="md"
+                {...form.getInputProps("password")}
+            />
+            {showConfirmPassword && (
+                <>
+                    <PasswordInput
+                        label="Confirm Password"
+                        placeholder="Confirm your password"
+                        withAsterisk
+                        mt="md"
+                        {...form.getInputProps("confirmPassword")}
+                    />
+                </>
+            )}
+            <br />
+            <IconButton type="submit" fullWidth loading={isLoading} icon="save" buttonText={submitLabel} />
+        </form>
+    )
+}
