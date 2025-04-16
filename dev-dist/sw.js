66b2846e7e19a2d00e73a479a5bff5ad336fd6fd
Alex <102932289+alexflightlessbird@users.noreply.github.com>
2025-04-16 17:58:18 +0000

    update mantine

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gjj3dqesjeo"
+    "revision": "0.bg1smk6q36o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
