8184c42ae1a017397c74b8478ed90a341c57d864
Alex <102932289+alexflightlessbird@users.noreply.github.com>
2025-06-26 17:42:22 +0000

    [skip render] update Test.jsx

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,42 +1,61 @@
+import React, { useState } from "react";
 import { setDocumentTitle } from "../utils/helpers";
 import { AddIcon, EditIcon, DeleteIcon } from "../common/components/ActionIcons";
-import { Box, Flex, Text, useMantineTheme } from "@mantine/core";
+import { NavLink, Group, Box, Flex, Text, Select, ActionIcon } from "@mantine/core";
 import IconButton from "../common/components/IconButton";
+import icons from "../utils/icons";
+import { useSearchParams } from "react-router-dom";
 
 export default function Test() {
-  const theme = useMantineTheme();
   setDocumentTitle({ title: "Test" });
 
-  console.log(Object.keys(theme.colors));
+  const [school, setSchool] = useState("uofm");
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  const colors = Object.keys(theme.colors);
+  function onSchoolChange (newSchool) {
+    setSchool(newSchool);
+    const newSearchParams = new URLSearchParams(searchParams);
+    newSearchParams.set("sch", newSchool);
+    setSearchParams(newSearchParams);
+  }
 
   return (
     <div>
       <h1>Testing Page</h1>
-      <IconButton buttonText="Testing" icon="add" />
-      {colors && (colors.map((color) => {
-        let boxProps = {};
-        if (color === "lightGray") boxProps.bg = theme.colors.darkBlue;
-        return (
-          <>
-          <Text>{color}</Text>
-            <Box key={color} {...boxProps}>
-              {Array.from({ length: 10 }).map((_, shade) => (
-                <Box key={color + shade}>
-                  <Text c={theme.colors[color][shade]}>{color} | {shade}</Text>
-                  <Flex>
-                    <AddIcon color={color} colorLoc={shade} />
-                    <EditIcon color={color} colorLoc={shade} />
-                    <DeleteIcon color={color} colorLoc={shade} />
-                  </Flex>
-                </Box>
-              ))}
-              <br />
-            </Box>
-          </>
-        )
-      }))}
+      <Flex maw="50%" direction="column">
+        <Group>
+          <Text>School:</Text>
+          <Select
+            variant="default"
+            data={[
+              { group: "Primary Admin", items: [{ value: "msu", label: "Michigan State University" }] },
+              { group: "Admin", items: [{ value: "uofm", label: "University of Michigan" }] },
+              { group: "Viewer", items: [] }
+            ]}
+            value={school}
+            onChange={onSchoolChange}
+            allowDeselect={false}
+          />
+        </Group>
+        <Flex maw="80%" direction="column">
+          <NavLink 
+            label="Dashboard"
+            leftSection={React.createElement(icons["dashboard"])}
+          />
+          <NavLink
+            label="Students"
+            leftSection={React.createElement(icons["user"])}
+          />
+          <NavLink
+            label="Teams"
+            leftSection={React.createElement(icons["team"])}
+          />
+          <NavLink
+            label="Tournaments"
+            leftSection={React.createElement(icons["trophy"])}
+          />
+        </Flex>
+      </Flex>
     </div>
   );
 }
