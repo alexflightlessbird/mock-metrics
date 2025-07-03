4829f76a9ab743cb0e9407d49a52c461b459671d
Alex <102932289+alexflightlessbird@users.noreply.github.com>
2025-07-03 18:12:52 +0000

    refractor UsersManagement

---
+++ src/features/adminDashboard/usersManagement/components/SearchControls.jsx
@@ -0,0 +1,24 @@
+import SearchBar from "../../../../common/components/SearchBar";
+
+export default function SearchControls ({
+    value,
+    onChange,
+    placeholder,
+    columns,
+    selectedColumn,
+    onColumnChange,
+    onReset
+}) {
+    return (
+        <SearchBar
+            value={value}
+            onChange={onChange}
+            placeholder={placeholder}
+            columns={columns}
+            selectedColumn={selectedColumn}
+            onColumnChange={onColumnChange}
+            onReset={onReset}
+            addEnabled={false}
+        />
+    )
+}
---
+++ src/features/adminDashboard/usersManagement/hooks/useUsersFilter.js
@@ -0,0 +1,22 @@
+import { useMemo } from "react";
+
+export default function useUsersFilter ({ users, searchTerm, searchColumn }) {
+    return useMemo(() => {
+        if (!users) return [];
+        if (!searchTerm) return users;
+
+        const lowerSearchTerm = searchTerm.toLowerCsae();
+
+        if (searchColumn === "all") {
+            return users.filter(user =>
+                user.id.toString().toLowerCase().includes(lowerSearchTerm) ||
+                user.email.toString().toLowerCase().includes(lowerSearchTerm) ||
+                user.name.toString().toLowerCase().includes(lowerSearchTerm)
+            )
+        };
+
+        return users.filter(user =>
+            String(user[searchColumn]).toLowerCase().includes(lowerSearchTerm)
+        );
+    }, [users, searchTerm, searchColumn]);
+}
