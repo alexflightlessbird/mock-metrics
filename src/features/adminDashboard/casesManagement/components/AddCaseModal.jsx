02d7ef608be0cfacdbf551d21c4dee1e8dca7bfe
Alex <102932289+alexflightlessbird@users.noreply.github.com>
2025-07-05 21:04:14 -0400

    adjust cases management area constraints

--- src/features/adminDashboard/casesManagement/components/AddCaseModal.jsx
+++ src/features/adminDashboard/casesManagement/components/AddCaseModal.jsx
@@ -1,95 +1,117 @@
-import { Modal, TextInput, Select, Checkbox, Button, Space, Input, NumberInput } from "@mantine/core";
+import {
+  Modal,
+  TextInput,
+  Select,
+  Checkbox,
+  Button,
+  Space,
+  Input,
+  NumberInput,
+} from "@mantine/core";
 import { useState } from "react";
 
-export default function AddCaseModal ({ opened, onClose, onSubmit }) {
-    const [isLoading, setIsLoading] = useState(false);
-    const [formValues, setFormValues] = useState({
+export default function AddCaseModal({ opened, onClose, onSubmit }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [formValues, setFormValues] = useState({
+    name: "",
+    year: new Date().getFullYear(),
+    type: "civil",
+    area: "",
+    is_active: true,
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formValues.name.trim()) return;
+
+    setIsLoading(true);
+
+    try {
+      await onSubmit(formValues);
+      setFormValues({
         name: "",
         year: new Date().getFullYear(),
         type: "civil",
         area: "",
-        is_active: true
-    });
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        if (!formValues.name.trim()) return;
-
-        setIsLoading(true);
-
-        try {
-            await onSubmit(formValues);
-            setFormValues({ name: "", year: new Date().getFullYear(), type: "civil", area: "", is_active: true });
-            onClose();
-        } finally {
-            setIsLoading(false);
-        }
+        is_active: true,
+      });
+      onClose();
+    } finally {
+      setIsLoading(false);
     }
+  };
 
-    return (
-        <Modal
-            opened={opened}
-            onClose={onClose}
-            title="Add Case"
-            centered
-            withCloseButton
-            overlayProps={{ backgroundOpacity: 0.4, blur: 3 }}
-        >
-            <form onSubmit={handleSubmit}>
-                <TextInput
-                    value={formValues.name}
-                    onChange={(e) => setFormValues(v => ({...v, name: e.target.value}))}
-                    label="Case Name"
-                    required
-                />
-                <Space h="xs" />
-                <NumberInput
-                    value={formValues.year}
-                    onChange={(e) => setFormValues(v => ({...v, year: e}))}
-                    label="Year"
-                    required
-                    min={1980}
-                    max={new Date().getFullYear()}
-                />
-                <Space h="xs" />
-                <Select
-                    data={[
-                        { value: "civil", label: "Civil" },
-                        { value: "criminal", label: "Criminal" }
-                    ]}
-                    allowDeselect={false}
-                    value={formValues.type}
-                    onChange={(e) => setFormValues(v => ({...v, type: e}))}
-                    label="Type"
-                />
-                <Space h="xs" />
-                <Select
-                    data={[
-                        { value: "", label: "None" },
-                        { value: "Invitationals/Regionals/ORCS", label: "Invitationals/Regionals/ORCS" },
-                        { value: "Nationals", label: "Nationals" },
-                        { value: "Rookie Rumble", label: "Rookie Rumble" },
-                        { value: "OLT", label: "OLT" },
-                        { value: "Other", label: "Other" }
-                    ]}
-                    allowDeselect={false}
-                    value={formValues.area}
-                    onChange={(e) => setFormValues(v => ({...v, area: e}))}
-                    label="Area"
-                />
-                <Space h="xs" />
-                <Input.Wrapper label="Active Status" />
-                <Checkbox
-                    checked={formValues.is_active}
-                    onChange={(e) => setFormValues(v => ({...v, is_active: e.target.checked}))}
-                    label={`${formValues.is_active ? "Active" : "Inactive" }`}
-                />
-                <Space h="xs" />
-                <Button type="submit" loading={isLoading}>
-                    Submit
-                </Button>
-            </form>
-        </Modal>
-    )
+  return (
+    <Modal
+      opened={opened}
+      onClose={onClose}
+      title="Add Case"
+      centered
+      withCloseButton
+      overlayProps={{ backgroundOpacity: 0.4, blur: 3 }}
+    >
+      <form onSubmit={handleSubmit}>
+        <TextInput
+          value={formValues.name}
+          onChange={(e) =>
+            setFormValues((v) => ({ ...v, name: e.target.value }))
+          }
+          label="Case Name"
+          required
+        />
+        <Space h="xs" />
+        <NumberInput
+          value={formValues.year}
+          onChange={(e) => setFormValues((v) => ({ ...v, year: e }))}
+          label="Year"
+          required
+          min={1980}
+          max={new Date().getFullYear()}
+        />
+        <Space h="xs" />
+        <Select
+          data={[
+            { value: "civil", label: "Civil" },
+            { value: "criminal", label: "Criminal" },
+          ]}
+          allowDeselect={false}
+          value={formValues.type}
+          onChange={(e) => setFormValues((v) => ({ ...v, type: e }))}
+          label="Type"
+        />
+        <Space h="xs" />
+        <Select
+          data={[
+            { value: "", label: "None" },
+            {
+              value: "invitationals/regionals/orcs",
+              label: "Invitationals/Regionals/ORCS",
+            },
+            { value: "nationals", label: "Nationals" },
+            { value: "rookie rumble", label: "Rookie Rumble" },
+            { value: "olt", label: "OLT" },
+            { value: "other", label: "Other" },
+          ]}
+          allowDeselect={false}
+          value={formValues.area}
+          onChange={(e) => setFormValues((v) => ({ ...v, area: e }))}
+          label="Area"
+        />
+        <Space h="xs" />
+        <Input.Wrapper label="Active Status" />
+        <Checkbox
+          checked={formValues.is_active}
+          onChange={(e) =>
+            setFormValues((v) => ({ ...v, is_active: e.target.checked }))
+          }
+          label={`${formValues.is_active ? "Active" : "Inactive"}`}
+        />
+        <Space h="xs" />
+        <Button type="submit" loading={isLoading}>
+          Submit
+        </Button>
+      </form>
+    </Modal>
+  );
 }
