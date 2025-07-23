169f827d7a66f08ce421528cc9b364cb1f1e782b
Alex <102932289+alexflightlessbird@users.noreply.github.com>
2025-07-23 15:49:15 +0000

    add dropdown for schools and adjust accessibility

--- src/layouts/NavLayout.jsx
+++ src/layouts/NavLayout.jsx
@@ -7,6 +7,8 @@ import {
   rem,
   useMantineTheme,
   Text,
+  Flex,
+  Menu,
 } from "@mantine/core";
 import {
   useDisclosure,
@@ -14,12 +16,13 @@ import {
   useLocalStorage,
   useHeadroom,
 } from "@mantine/hooks";
-import { LuBriefcase, LuSchool, LuLogOut, LuDatabase } from "react-icons/lu";
+import { LuBriefcase, LuSchool, LuLogOut, LuDatabase, LuChevronsUpDown as ChevronIcon, LuLayoutDashboard } from "react-icons/lu";
 import { PiGavelFill } from "react-icons/pi";
 import { useNavigate, useLocation } from "react-router-dom";
 import { emToPx } from "../common/utils/helpers";
 import { useAuth } from "../context/AuthContext";
 import Logo from "../assets/svgs/Logo";
+import { useUserAssignments } from "../features/dashboard/hooks/useUserAssignments";
 
 const NAV_LINKS = [
   { icon: PiGavelFill, label: "Dashboard", path: "/" },
@@ -33,7 +36,22 @@ export default function NavLayout({ children }) {
   const location = useLocation();
   const pinned = useHeadroom({ fixedAt: 0, behavior: "transform" });
 
-  const { signOut, isSuperAdmin, loading, superAdminLoading } = useAuth();
+  const { user, signOut, isSuperAdmin, loading, superAdminLoading } = useAuth();
+
+  const { assignments, isLoading } = useUserAssignments(user.id);
+
+  const [selectedSchoolId, setSelectedSchoolId] = useLocalStorage({
+    key: "school",
+    defaultValue: null
+  });
+
+  useEffect(() => {
+    if (
+      assignments?.length === 1 &&
+      assignments[0]?.school_id !== selectedSchoolId
+    )
+      setSelectedSchoolId(assignments[0]?.school_id);
+  }, [assignments]);
 
   const { width } = useViewportSize();
   const [mobileOpened, { toggle: toggleMobile, close: closeMobile }] =
@@ -50,6 +68,18 @@ export default function NavLayout({ children }) {
   });
 
   const BOTTOM_LINKS = [
+    {
+      enabled: isSuperAdmin && !superAdminLoading,
+      icon: LuLayoutDashboard,
+      label: "Admin Test",
+      path: "/admin-test",
+      onClick: () => {
+        navigate("/admin-test");
+        setTimeout(() => {
+          closeMobile();
+        }, 100);
+      }
+    },
     {
       enabled: isSuperAdmin && !superAdminLoading,
       icon: LuDatabase,
@@ -131,6 +161,8 @@ export default function NavLayout({ children }) {
     };
   }, [isResizing, desktopCollapsed]);
 
+  if (isLoading) return <></>;
+
   return (
     <AppShell
       header={{ height: 70, collapsed: !pinned }}
@@ -156,7 +188,7 @@ export default function NavLayout({ children }) {
             size="sm"
             style={{ position: "absolute", left: "var(--mantine-spacing-md)" }}
           />
-          <Group h="100%" gap="0">
+          <Group h="100%" gap="0" tabIndex="0" onKeyDown={(e) => { if (e.key === "Enter" || e.key === " ") { e.preventDefault(); e.currentTarget.click(); } }} style={{ userSelect: "none", webkitUserSelect: "none", cursor: "pointer" }} onClick={() => navigate("/")}>
             <Logo />
             <Text ff="Trirong" fz="xl">
               MockMetrics
@@ -180,9 +212,70 @@ export default function NavLayout({ children }) {
             width: "100%",
           }}
         >
+          <AppShell.Section mb="lg">
+            {assignments.length > 0 && (
+              <>
+                {!(desktopCollapsed && !isMobile) && <Text size={isMobile ? "md" : "sm"} mb="2">Current School:</Text>}
+                <Menu
+                  withinPortal
+                  position="bottom-start"
+                  width={isMobile ? `calc(100vw - (${theme.spacing.xs} * 3))` : 200}
+                  disabled={assignments.length === 1}
+                >
+                  <Menu.Target>
+                    <Flex
+                      bg={theme.colors.gray[1]}
+                      pl="xs"
+                      pr="xs"
+                      pt="xs"
+                      pb="xs"
+                      bdrs={`calc(${theme.spacing.xs} - 2px)`}
+                      style={{
+                        overflow: "hidden",
+                        cursor: assignments.length > 1 ? "pointer" : "default",
+                        alignItems: "center",
+                        justifyContent: "space-between"
+                      }}
+                      tabIndex={assignments.length > 1 ? 0 : ""}
+                      onKeyDown={(e) => {
+                        if (assignments.length > 1 && (e.key === "Enter" || e.key === " ")) {
+                          e.preventDefault();
+                          e.currentTarget.click();
+                        }
+                      }}
+                    >
+                      <Text truncate size={isMobile ? "md" : "xs"}>
+                        {desktopCollapsed && !isMobile
+                          ? assignments.find(a => a.school_id === selectedSchoolId)?.schools.short_name
+                          : assignments.find(a => a.school_id === selectedSchoolId)?.schools.name
+                        }
+                      </Text>
+                      {assignments.length > 1 && !(desktopCollapsed && !isMobile) && (
+                        <ChevronIcon />
+                      )}
+                    </Flex>
+                  </Menu.Target>
+
+                  {assignments.length > 1 && (
+                    <Menu.Dropdown>
+                      {assignments.map((a) => (
+                        <Menu.Item
+                          key={a.school_id}
+                          onClick={() => setSelectedSchoolId(a.school_id)}
+                        >
+                          {a.schools.name} ({a.schools.short_name})
+                        </Menu.Item>
+                      ))}
+                    </Menu.Dropdown>
+                  )}
+                </Menu>
+              </>
+            )}
+          </AppShell.Section>
           <AppShell.Section style={{ flex: 1 }}>
             {NAV_LINKS.map((link) => (
               <NavLink
+                disabled={link.label.toLowerCase() === "school" && !selectedSchoolId}
                 key={link.path}
                 active={location.pathname === link.path}
                 label={
@@ -196,6 +289,17 @@ export default function NavLayout({ children }) {
                   }, 100);
                 }}
                 mb="xs"
+                tabIndex={link.label.toLowerCase() === "school" && !selectedSchoolId ? "" : 0}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter" || e.key === " ") {
+                    e.preventDefault();
+                    e.currentTarget.click();
+                    closeMobile();
+                  }
+                }}
+                style={{
+                  cursor: link.label.toLowerCase() === "school" && !selectedSchoolId ? "not-allowed" : undefined
+                }}
               />
             ))}
           </AppShell.Section>
@@ -215,6 +319,14 @@ export default function NavLayout({ children }) {
                         : link.label
                     }
                     leftSection={<link.icon size="1rem" />}
+                    tabIndex={0}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter" || e.key === " ") {
+                        e.preventDefault();
+                        e.currentTarget.click();
+                        closeMobile();
+                      }
+                    }}
                     onClick={() => link.onClick()}
                   />
                 )
